254f40ff3b3919cd653039402fd693018a904965
StephaneCash <kikonistephane@gmail.com>
2022-12-06 14:37:19 +0100

    insert btn print

---
+++ src/components/eleves/GetEtudiantsOrdre.jsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from 'react'
+import DetailEtudiant from './DetailEtudiant';
+
+function GetEtudiantsOrdre(props) {
+
+    let dataExcel = props.dataExcel;
+    let state = props.state;
+    let valMotif = props.valMotif;
+    let valueSearch = props.valueSearch;
+
+    const [showModalDetail, setShowModalDetail] = useState(false);
+
+    const [valueDetail, setValueDetail] = useState({});
+
+    const handleShowModal = (value) => {
+        setValueDetail(value);
+        setShowModalDetail(true);
+    }
+
+    const closeModal = () =>{
+        setShowModalDetail(false);
+    }
+
+    useEffect(() => {
+        if (state === undefined && state === null) {
+            state = '';
+        }
+    }, []);
+
+    return (
+        <>
+            {dataExcel && dataExcel.data ? (
+                dataExcel.data.filter((data) => {
+                    return data.filiere.includes(state ? state.val : "") && data.motif.includes(valMotif ? valMotif : "") && data.numeroRef.includes(valueSearch ? valueSearch : "")
+                }).map((bank, i) => {
+                    return (
+                        <tr key={i}>
+                            <td>{i + 1}</td>
+                            <td>{bank.nom}</td>
+                            <td>{bank.postnom}</td>
+                            <td>{bank.prenom}</td>
+                            <td>{bank.sexe}</td>
+                            <td>
+                                {bank.filiere}
+                            </td>
+                            <td>{valueSearch ? bank.numeroRef : "********"}</td>
+                            <td>{bank.montant}</td>
+                            <td>{bank.motif}</td>
+
+                            <td style={{ width: "140px" }}>
+                                <button className='btn btn-success' onClick={() => handleShowModal(bank)}>
+                                    <i className='fa fa-info'></i> Détail
+                                </button>
+                            </td>
+                        </tr>
+                    )
+                })
+
+            ) :
+                <tr>
+                    <td colSpan='10px' className='text-center'>
+                        <i className='fa fa-spinner fa-pulse fa-2x'></i> Chargement...
+                    </td>
+                </tr>
+            }
+            <DetailEtudiant showModalDetail={showModalDetail} closeModal={closeModal} valueDetail={valueDetail}/>
+        </>
+    )
+}
+
+export default GetEtudiantsOrdre
--- src/pages/EtudiantsEnOrdre.js
+++ src/pages/EtudiantsEnOrdre.js
@@ -1,28 +1,53 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
-import { NavLink } from 'react-router-dom';
-import GetDataBank from '../components/eleves/GetDataBank';
+import { useLocation } from 'react-router-dom';
+import GetEtudiantsOrdre from '../components/eleves/GetEtudiantsOrdre';
 import Leftbar from '../components/Leftbar'
 import Navbar from '../components/Navbar'
 import "../css/Eleves.css"
 
 function EtudiantsEnOrdre() {
-    const [data, setData] = useState([]);
-    const [value, setValue] = useState("");
+    const [dataExcel, setDataExcel] = useState([]);
 
-    const getAllEtudiants = () => {
+    const [valueSearch, setValue] = useState('');
+    const [etatMotif, setEtatMotif] = useState(false);
+    const [id, setId] = useState(null);
+    const [valMotif, setValMotif] = useState('');
+    const [dataUniques, setDataUniques] = useState([]);
+
+    const location = useLocation();
+    const { state } = location;
+
+    const getAllExcelData = () => {
         axios.get('http://localhost:5000/api/read')
             .then(res => {
-                setData(res.data);
+                setDataExcel(res.data);
             })
             .catch(err => {
                 console.log(err.response)
             })
     };
 
+    const handleMotif = (val) => {
+        setEtatMotif(!etatMotif);
+        setId(val)
+        setValMotif(val);
+    };
+
     useEffect(() => {
-        getAllEtudiants();
+        getAllExcelData();
     }, []);
+
+    useEffect(() => {
+        let array = [];
+        if (dataExcel) {
+            dataExcel && dataExcel.data && dataExcel.data.map((value) => {
+                array.push(value.motif)
+                let valuesUniques = [...new Set(array)]
+                setDataUniques(valuesUniques);
+            })
+        }
+    }, [dataExcel]);
 
     return (
         <div>
@@ -34,24 +59,34 @@ function EtudiantsEnOrdre() {
                 <div className='col-sm-10'>
                     <div className='getAllEleves' style={{ padding: "1rem" }}>
                         <div className="alert alert-success">
-                            <h6>Données de payement des étudiants en ordre <i className="fa fa-graduation-cap"></i></h6>
-                            Nombre des étudiants {data && data.data !== undefined && data.data !== null ? <>( {data.data.length} )</> : "Pas de données"}
+                            <h6>Liste des étudiants classés par motif de payement en ordre <i className="fa fa-graduation-cap"></i></h6>
+                            Nombre des étudiants {dataExcel && dataExcel.data !== undefined ? <>( {state ? state.num : dataExcel.data.length} )</> : "Pas de données"}
+                        </div>
+                        <div className="alert alert-info grille">
+                            {
+                                dataUniques && dataUniques ?
+                                    dataUniques.map((val, i) => {
+                                        return (
+                                            <div className='' key={i}>
+                                                <button className={id === val ? 'btn btn-info motifSelected' : 'btn btn-info'}
+                                                    onClick={() => handleMotif(val)}>
+                                                    {val}
+                                                </button> {' '}
+                                            </div>
+                                        )
+                                    }) : <i className='text-dark'>"Pas de données"</i>
+                            }
                         </div>
                         <div className='d-flex border p-2'>
                             <div className="col-sm-8">
                                 <div className='col-sm-5'>
-                                    <input type="search" className="form-control" placeholder='Rechercher par numéro de référence'
+                                    <input type="search" className="form-control"
+                                        placeholder='Rechercher par numéro de référence'
                                         onChange={(e) => setValue(e.target.value)} />
                                 </div>
                             </div>
-                            <div className="col-sm-4">
-                                <NavLink to="addFile">
-                                    <button style={{ float: "right" }} className='btn btn-primary'>
-                                        Importer un fichier</button>
-                                </NavLink>
-                            </div>
                         </div>
-                        <table className='table table-striped table-bordered mt-3'>
+                        <table className='table table-striped table-bordered'>
                             <thead>
                                 <tr>
                                     <th>N°</th>
@@ -60,13 +95,14 @@ function EtudiantsEnOrdre() {
                                     <th>Prénom</th>
                                     <th>Sexe</th>
                                     <th>Filière</th>
-                                    <th>Numéro Réf</th>
+                                    <th>N° Référence</th>
                                     <th>Montant</th>
                                     <th>Motif</th>
+                                    <th>Actions</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                <GetDataBank data={data} valueSearch={value} />
+                                <GetEtudiantsOrdre dataExcel={dataExcel} state={state} valueSearch={valueSearch} valMotif={valMotif} />
                             </tbody>
                         </table>
                     </div>
